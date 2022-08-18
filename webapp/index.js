8794405d681f506ebd73eaf2c17e4751a089e7a1
Ilya Khayarov <ilya.ssb.ru@mail.ru>
2022-08-18 20:03:13 +0500

    Step 3: Controls

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,7 @@
 sap.ui.define([
-    
-], function() {
+    "sap/m/Text"
+], function(Text) {
     'use strict';
     
-    alert("UI5 is ready to go!");
+    new Text({ text: "Hello UI5 one more!"}).placeAt("content");
 });
